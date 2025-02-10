2cdc6fa1afeff3aa8922fca65fab1229ca7dc67c
atompsv <tomtomtom.tt953@gmail.com>
2025-02-10 20:45:06 +0700

    Add description when getting request

--- packages/foundry/contracts/Payme.sol
+++ packages/foundry/contracts/Payme.sol
@@ -54,33 +54,36 @@ contract Payme {
         Request storage request = requestInfo[id];
         require(request.responder == msg.sender, "permission denied");
         require(request.status == uint8(Status.Pending) || request.status == uint8(Status.Rejected), "request is not peding or already completed");
-        require(msg.value == request.amount, "mismatch amount");
+        // require(msg.value == request.amount, "mismatch amount");
+        require(msg.value > 0, "amount should be greater than zero");
 
         payable(request.asker).transfer(msg.value);
         request.status = uint8(Status.Completed);
+        request.amount = msg.value;
 
         // emit
         emit  RequestUpdated(request.id, request.asker, request.responder, request.amount, request.status);
     }
 
-
-    function rejectRequest(uint id) public {
+    function rejectRequest(uint id, string memory desc) public {
         require(id > 0  && id < nextId, "invalid request id");
+        require(bytes(desc).length <= MAX_DESCRIPTION, "description is too long");
 
         Request storage request = requestInfo[id];
         require(request.responder == msg.sender, "permission denied");
         require(request.status == uint8(Status.Pending) || request.status == uint8(Status.Completed), "request is not pending or already rejected");
 
         request.status = uint8(Status.Rejected);
+        request.description = desc;
 
         // emit
         emit  RequestUpdated(request.id, request.asker, request.responder, request.amount, request.status);
     }
 
 
-    function getRequestById(uint id) public  view returns (address asker, address responder, uint amount, uint8 status,uint256 createdAt){
+    function getRequestById(uint id) public  view returns (address asker, address responder, uint amount, uint8 status, string memory desc, uint256 createdAt){
         require(id > 0  && id < nextId, "invalid request id");
-        return (requestInfo[id].asker, requestInfo[id].responder, requestInfo[id].amount, requestInfo[id].status, requestInfo[id].createdAt);
+        return (requestInfo[id].asker, requestInfo[id].responder, requestInfo[id].amount, requestInfo[id].status, requestInfo[id].description, requestInfo[id].createdAt);
     }
 
     function getBillsByAddress(address user) public view returns (uint[] memory) {
